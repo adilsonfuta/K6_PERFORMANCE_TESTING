931c9b44cd986cdde5e322d90573e92fe26f8d86
adilsonfuta <adilson.futa@jupiter.co.ao>
2022-11-28 15:07:12 +0100

    teste

--- runner.js
+++ runner.js
@@ -1,9 +1,14 @@
-require('dotenv').config();
+// require('dotenv').config();
 import http from 'k6/http';
 import {sleep} from 'k6';
 // const url = process.env.URL_LOAD;
 
+export const options = {
+    vus: 5,
+    duration: '30s',
+  };
+
 export default function () {
-    http.get(url);
+    http.get("https://test.k6.io/");
     sleep(1);
 }
